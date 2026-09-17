aaa171611d20887873de3442b772c3b0763cefca
Vsevolod Ozmitel Volpato <sevino2004@gmail.com>
2026-09-17 17:51:14 +0200

    Update CSV file path for flux profile plotting

--- analysis/plot_flux_profile.py
+++ analysis/plot_flux_profile.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(r"C:\Users\sevin\OneDrive\Desktop\Uni\CppProgetti\Fermilab 2.0\bin_fluc_unc.csv")
+df = pd.read_csv(r"bin_fluc_unc.csv")
 
 plt.errorbar(df["bin center"], df["flux"], yerr=df["uncertainty"], fmt='o-', capsize=3, markersize=3)
 plt.xlabel("radial distance from axis  (cm)")
